056846b5a1d5559a6eef29699d39194a00e37451
Kaushal Kumar <90971842+kaushal32@users.noreply.github.com>
2021-11-30 21:55:08 +0530

    Threading Concept

--- src/Threading.java
+++ src/Threading.java
@@ -2,7 +2,7 @@ public class Threading {
     public static void main(String[] args) {
         Thread obj = Thread.currentThread();
         System.out.println(obj.getName());
-        obj.setName("Anuruddh");
+        obj.setName("Kaushal");
         obj.setPriority(Thread.MIN_PRIORITY);
         System.out.println(obj.getId());
         System.out.println(obj.isAlive());
