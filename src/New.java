19e890f9da485d14f94cd644aab442414815ca56
Kaushal Kumar <90971842+kaushal32@users.noreply.github.com>
2021-11-30 21:58:07 +0530

    Basic Ques.

---
+++ src/New.java
@@ -0,0 +1,10 @@
+public class New {
+    public void cool() {
+        int a=10;
+    }
+
+    public static void main(String[] args) {
+        New obj = new New();
+        System.out.println(obj);
+    }
+}
